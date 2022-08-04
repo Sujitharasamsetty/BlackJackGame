862d3a5854a52f27c19129b648a881b709004fbc
sujitharasamsetty.sujitha=Rasamsetty <sujitharasamsetty@gmail.com>
2022-08-04 21:13:54 +0530

    Card Scores added

--- Blackjack.py
+++ Blackjack.py
@@ -1,3 +1,20 @@
 from flask import Flask
 from flask import render_template
 import random
+
+app=Flask(__name__)
+
+
+CARD_SCORES = {
+    '6': 6,
+    '7': 7,
+    '8': 8,
+    '9': 9,
+    '10': 10,
+    'Valet': 2,
+    'Dama': 3,
+    'Korol': 4,
+    'Tuz': 11
+}
+
+
